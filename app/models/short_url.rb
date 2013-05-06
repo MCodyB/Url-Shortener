4d7582272189f320e43089677797e6db3f444a06
Cody+Natasha <cody+natasha@appacademy.io>
2013-05-06 14:10:06 -0400

    First method added, deleted duplicate migration files

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,9 +4,19 @@ class ShortUrl < ActiveRecord::Base
   belongs_to :user
   belongs_to :long_url
 
+  validates :url, :uniqueness => true
+
   validates :long_url_id, :user_id, :presence => true
 
   has_many :visits
   has_many :taggings
   has_many :tag_topics, :through => :taggings
+
+
+  def self.generate_short_url(long_url_id, user_id)
+    str = "#{SecureRandom.urlsafe_base64(6)}"
+    self.create(url: str, user_id: user_id, long_url_id: long_url_id, comments: nil)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates :name, :uniqueness => true
 
+  validates :name, :email, :presence => true
+
   has_many :long_urls, :through => :short_urls
   has_many :visits, :through => :short_urls
 end
